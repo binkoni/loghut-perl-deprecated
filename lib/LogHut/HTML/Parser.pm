e1d6ec4547610a497c83a6340bec2196ccd1f443
Byeonggon Lee <gonny952@gmail.com>
2015-01-07 10:08:51 +0900

    Updated LogHut::HTML::Parser

--- lib/LogHut/HTML/Parser.pm
+++ lib/LogHut/HTML/Parser.pm
@@ -55,7 +55,7 @@ sub parse {
         $self->{debug} and sleep 1;
         $self->{debug} and say $root_contents;
         if($self->{lines} =~ m/\G<\/(?<tag>[0-9A-z]+)>/cgi) {
-            $self->{debug} and $self->debug("multiline closing tag $+{tag}");
+            $self->{debug} and $self->debug("---->multiline closing tag ($+{tag})");
             if($root_name eq $+{tag}) {
                 $root->set_value('contents', $root_contents);
                 return $root;
@@ -70,47 +70,65 @@ sub parse {
                 $self->{debug} and $self->debug('comment');
                 $root_contents .= $&;
                 if($self->{lines} =~ m/\G(?<comment_contents>.*?)-->\s*/cgis) {
-                    $self->{debug} and $self->debug("comment contents $+{comment_contents} and closing comment");
+                    $self->{debug} and $self->debug("---->comment contents ($+{comment_contents}) and comment closed");
                     $root_contents .= $&;
                 }
-            } elsif($self->{lines} =~ m/\G(?<tag>doctype|entity)/cgi) {
-                $self->{debug} and $self->debug('doctype|entity');
+            } elsif($self->{lines} =~ m/\G(?<tag>doctype|entity)(\s+(?<attributes>[^>]*?))?>\s*/cgi) {
+                $self->{debug} and $self->debug($+{tag});
+                $self->{debug} and $self->debug("---->attributes ($+{attributes}) and ($+{tag}) closed");
                 $root_contents .= $&;
-                if($self->{lines} =~ m/\G(?<attributes>[^>]*?)>\s*/cgi) {
-                    $self->{debug} and $self->debug("attributes $+{attributes} and closing doctype|entity $&");
-                    $root_contents .= $&;
-                }
             } 
         } elsif($self->{lines} =~ m/\G<(?<tag>[0-9A-z]+)(\s+(?<attributes>[^>]*?))?\/>\s*/cgi) {
-            $self->{debug} and $self->debug("inline elemnt $1, attributes $2");
+            $self->{debug} and $self->debug("---->inline elemnt ($+{tag}), attributes ($+{attributes})");
             $root_contents .= $&;
             $child = LogHut::Data::TreeNode->new();
             $child->set_value('tag', $+{tag});
             $attributes = $+{attributes};
-            while($attributes =~ m/(?<attribute_name>\w+) *\= *(?<q>['"])(?<attribute_value>.*?)\k<q>/gs) {
-                $child->set_value($+{attribute_name}, $+{attribute_value});
+            while(1) {
+                if($attributes =~ m/\G(?<attribute_name>\S+?)\s*=\s*(?<q>['"])(?<attribute_value>.+?)\k<q>\s*/cgis) {
+                    $self->{debug} and $self->debug("---->attribute_name : ($+{attribute_name}) attribute_value : ($+{attribute_value})");
+                    $child->set_value($+{attribute_name}, $+{attribute_value});
+                } elsif($attributes =~ m/\G(?<attribute_name>\S+)\s*/cgi) {
+                    $child->set_value($+{attribute_name}, undef);
+                } else {
+                    last;
+                }
             }
         } elsif($self->{lines} =~ m/\G<(?<tag>style|script)(\s+(?<attributes>[^>]*?))?>(?<plain_contents>.*?)<\/\k<tag>>\s*/cgis) { 
             
-            $self->{debug} and $self->debug("multiline style|script tag $+{tag}, attributes $+{attributs} plain contents $+{plain_contents} ");
+            $self->{debug} and $self->debug("---->multiline tag ($+{tag}), attributes ($+{attributs}) plain contents $+{plain_contents} ");
             $root_contents .= $&;
             $child = LogHut::Data::TreeNode->new();
             $child->set_value('tag', $+{tag});
             $child->set_value('contents', $+{plain_contents});
             $attributes = $+{attributes};
-            while($attributes =~ m/(?<attribute_name>\w+) *\= *(?<q>['"])(?<attribute_value>.*?)\k<q>/gs) {
-                $child->set_value($+{attribute_name}, $+{attribute_value});
+            while(1) {
+                if($attributes =~ m/\G(?<attribute_name>\S+?)\s*=\s*(?<q>['"])(?<attribute_value>.+?)\k<q>\s*/cgis) {
+                    $self->{debug} and $self->debug("---->attribute_name : ($+{attribute_name}) attribute_value: ($+{attribute_value})");
+                    $child->set_value($+{attribute_name}, $+{attribute_value});
+                } elsif($attributes =~ m/\G(?<attribute_name>\S+)\s*/cgi) {
+                    $child->set_value($+{attribute_name}, undef);
+                } else {
+                    last;
+                }
             }
             $root->add_child($child);
             
         } elsif($self->{lines} =~ m/\G<(?<tag>[0-9A-z]+)(\s+(?<attributes>[^>]*?))?>\s*/cgi) {
-            $self->{debug} and $self->debug("multiline opening tag $+{tag}, attributes $+{attributes}");
+            $self->{debug} and $self->debug("---->multiline opening tag ($+{tag}), attributes ($+{attributes})");
             $root_contents .= $&;
             $child = LogHut::Data::TreeNode->new();
             $child->set_value('tag', $+{tag});
             $attributes = $+{attributes};
-            while($attributes =~ m/(?<attribute_name>\w+) *\= *(?<q>['"])(?<attribute_value>.*?)\k<q>/gs) {
-                $child->set_value($+{attribute_name}, $+{attribute_value});
+            while(1) {
+                if($attributes =~ m/\G(?<attribute_name>\S+?)\s*=\s*(?<q>['"])(?<attribute_value>.+?)\k<q>\s*/cgis) {
+                    $self->{debug} and $self->debug("---->attribute_name : ($+{attribute_name}) attribute_value : ($+{attribute_value})");
+                    $child->set_value($+{attribute_name}, $+{attribute_value});
+                } elsif($attributes =~ m/\G(?<attribute_name>\S+)\s*/cgi) {
+                    $child->set_value($+{attribute_name}, undef);
+                } else {
+                    last;
+                }
             }
             $child_result =  $self->parse($child);
             $root->add_child($child);
@@ -122,46 +140,13 @@ sub parse {
                 return $root;
             }
         } elsif($self->{lines} =~ m/\G(?<plain_contents>[^<]+)/cgi) {
-            $self->{debug} and $self->debug("plain contents $+{plain_contents}");
+            $self->{debug} and $self->debug("---->plain contents ($+{plain_contents})");
             $root_contents .= $&;
         } else {
             $root->set_value('contents', $root_contents);
             return $root;
         }
     }
-
-#    if(<) {
-#        if(script or style) #special elements
-#            if(attributes)
-#                #process attributes
-#            else #no attributes
-#            if(/>)
-#            elsif(>)
-#                if(plain contents)
-#                    #but what if (< again)? --> You don't need to worry about opening tag you can just read until closing tag
-#                if(</script> or </style>)
-#        else #normal elements
-#            if(attributes)
-#                #process attribues
-#            else #no attributes
-#            if(/>)
-#            elsif(>)
-#                if(plain contents)
-#                    #but what if (< again)? --> I don't know yet..
-#                if(</ >)
-
-#    }
-
-#    if(<!) {
-#        if(DOCTYPE,ENTITY)
-#        if(--)
-#            if(--!>
-#        if([CDATA[)
-#            if(]]>)
-#    }
-
-#    if(</) {
-#    }
 }
 
 sub debug {
